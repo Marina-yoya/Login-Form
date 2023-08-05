78287604a05f68dbe3e367f0ae83706dbf67de3f
Marina-yoya <64007447+Marina-yoya@users.noreply.github.com>
2023-08-05 11:41:46 +0100

    button gradient mixin and ripple effect

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".btn").click(function(){
-        console.log("click")
-    })
+
+    function RippleStyle(width, height, posX, posY) {
+        this.width = (width <= height) ? height : width;
+        this.height = (width <= height) ? height : width;
+        this.top = posY - (this.height * 0.5);
+        this.left = posX - (this.width * 0.5);
+    }
+
+    $('.btn').on('mousedown', function (e) {
+
+        let rippleEl = $('<span class="btn-ripple"></span>').appendTo(this);
+        console.log("clicked")
+        console.log(rippleEl)
+
+        let pos = $(this).offset();
+        let width = $(this).outerWidth();
+        let height = $(this).outerHeight();
+        let posX = e.pageX - pos.left;
+        let posY = e.pageY - pos.top;
+        let rippleStyle = new RippleStyle(width, height, posX, posY);
+        rippleEl.css(rippleStyle);
+    });
+
+    $('.btn').on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', '.btn-ripple', function () {
+        $(this).remove();
+    });
 
 
 });
